def19e63b3b4b04d10e3462bf67c82372627b990
supriyayadav221 <supriyayadav221@gmail.com>
2021-04-22 23:54:26 +0530

    project submission

---
+++ programming.py
@@ -0,0 +1,21 @@
+
+'''
+2.Write a python program to check the user input abbreviation.
+If the user enters "lol", print "laughing out loud".
+If the user enters "rofl", print "rolling on the floor laughing".
+If the user enters "lmk", print "let me know".
+If the user enters "smh", print "shaking my head".
+'''
+str=input()
+str=str.lower()
+if(str =="lol" ):
+    print("laughing out loud")
+elif(str=="rofl"):
+    print("rolling on the floor laughing")
+elif(str=="lmk"):
+    print("let me know")
+elif(str=="smh"):
+    print("shaking my head")
+else:
+    print("")
+
