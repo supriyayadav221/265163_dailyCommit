c9ecf6d3b5ca21cb6bdb4230717dcddc14e726c2
supriyayadav221 <supriyayadav221@gmail.com>
2021-04-24 19:01:13 +0530

    repeated item in tuple

--- max and min of sets.py
+++
@@ -1,9 +0,0 @@
-def MAX(sets):
-    return (max(sets))
-
-def MIN(sets):
-    return (min(sets))
-
-sets = set([8, 16, 24, 1, 25, 3, 10, 65, 55])
-print("max in set :",MAX(sets))
-print("min in set :",MIN(sets))
